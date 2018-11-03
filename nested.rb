b481ab01b4d74faed563217f880a1b03976d7dcc
Jeffrey Guan <jeffreyg2240@hstat.org>
2018-11-03 00:34:33 +0000

    hw 12

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
  		}
     
   # CODE HERE
-    
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -40,7 +40,7 @@ def alan_kay_is_known_for
      
   # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   # CODE HERE
-     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -61,7 +61,7 @@ def dennis_ritchies_language
       }
     }
   # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages].first
 end
 
 def adding_matz
@@ -81,8 +81,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-    
-  # add the following information to the top level of programmer_hash
+    programmer_hash[Hash.new] = {
+      :yukihiro_matsumoto => {}
+    }
+    # add the following information to the top level of programmer_hash
   # KEY
   # :yukihiro_matsumoto
   # VALUE
@@ -111,7 +113,8 @@ def changing_alan
   
   # change what Alan Kay is :known_for to "GUI"
   # CODE HERE
-  
+   programmer_hash[:alan_kay][:known_for] = "GUI"
+   programmer_hash
 end
 
 def adding_to_dennis
@@ -134,5 +137,8 @@ def adding_to_dennis
   
   # adds 'Assembly' to Dennis Ritchie's languages
   # CODE HERE
-  
+   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+   programmer_hash
 end
+
+
