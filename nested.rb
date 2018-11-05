a5e3b099a278ecb7481f23eb226d93ed13328c80
Danny Wu <dannyw3292@hstat.org>
2018-11-05 11:53:47 +0000

    Complete assignment

--- nested.rb
+++ nested.rb
@@ -17,7 +17,7 @@ def hopper
  		}
     
   # CODE HERE
-    
+    programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -40,7 +40,7 @@ def alan_kay_is_known_for
      
   # What combination of keys would you use to return the value of the :known_for key of :alan_kay?
   # CODE HERE
-     
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -61,7 +61,8 @@ def dennis_ritchies_language
       }
     }
   # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages].join
+
 end
 
 def adding_matz
@@ -88,7 +89,12 @@ def adding_matz
   # VALUE
   # {:known_for => "Ruby", :languages => ["LISP, C"]}
   # CODE HERE
-  
+  programmer_hash [:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP, C"]
+  }
+  programmer_hash
+
 end
 
 def changing_alan
@@ -108,10 +114,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
+
   
   # change what Alan Kay is :known_for to "GUI"
   # CODE HERE
-  
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -134,5 +142,6 @@ def adding_to_dennis
   
   # adds 'Assembly' to Dennis Ritchie's languages
   # CODE HERE
-  
+  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash
 end
